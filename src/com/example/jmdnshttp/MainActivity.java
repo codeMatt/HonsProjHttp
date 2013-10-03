681ba72fa05215d06f1ec3559d0530421969983c
MATT <mattwat1@gmail.com>
2013-10-03 13:21:56 +0200

    added simple server initialisation

--- src/com/example/jmdnshttp/MainActivity.java
+++ src/com/example/jmdnshttp/MainActivity.java
@@ -1,29 +1,59 @@
 package com.example.jmdnshttp;
 
-import java.io.IOException;
-
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
 	private FileServer server; //80 is for root users on android
-	
+	private EditText textField;
+	private String localAddress;
+	private boolean started;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		started = false;
+		
 		//set up wifi manager and IP address
         WifiManager wifi = (WifiManager)
         		getSystemService(android.content.Context.WIFI_SERVICE);
         int ipAddress = wifi.getConnectionInfo().getIpAddress();
-		String localAddress = getIP(ipAddress);
+		localAddress = getIP(ipAddress);
 		
-		initServer(localAddress);
+		
+		//UI Features--------------------------------------------------------------------
+		//-------------------------------------------------------------------------------
+		textField = (EditText) findViewById (R.id.editText2);
+		
+		
+		Button startButton = (Button) findViewById (R.id.button1);
+		startButton.setOnClickListener(new OnClickListener() {       	 
+			@Override
+			public void onClick(View arg0) {
+				if(!started)
+					initServer(localAddress);	
+			} 
+		}); 
+		
+		
+		
+		
+		
+		//-------------------------------------------------------------------------------
+		//End UI Features----------------------------------------------------------------
+		
+
+		
+
 	}
 
 	@Override
@@ -52,7 +82,10 @@ public class MainActivity extends Activity {
 	private boolean initServer(String address){
 		if(address !=null){
 			server  = new FileServer(8080); //80 is reserved for root users on android
-						
+			
+			if(server!=null){
+				textField.append("The server has been started, at " + localAddress + ":8080");
+			}
 			
 		}
 		
